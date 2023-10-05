8681c2afbf2b3c866ee36561929f9425d0c79c02
parkjawonocu <jwonpark53@gmail.com>
2023-10-05 14:07:23 +0900

    Initial Commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by jwonpark on 2023/10/05.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
